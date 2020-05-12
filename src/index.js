d72a7e31a8bf6231cf8f1d77784a1bf62e927a02
Mitchell Schooler <mitchell@schooler.me>
2020-05-12 11:47:40 -0700

    It works!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const bouncy = require('bouncy');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,9 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  const { host } = req.headers;
-  const [domain, port] = host.split(':');
-});
+bouncy(function(req, bounce) {
+  if (req.headers.host.split(':')[0] === 'mitchell.schooler.me') {
+    bounce(3001);
+  }
+}).listen(port);
 
-app.listen(port, () => console.log('Listening on port', port));
+// app.listen(port, () => console.log('Listening on port', port));
